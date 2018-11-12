0aba3885d9daaac2c464ea6aa2d9c6fa11d4c607
SCHREIBERHV17 <SCHREIBERHV17@5CG73107TD.GCC.edu>
2018-11-12 10:31:42 -0500

    Second Attempt to Change Immovable to 2 M's

--- src/blockdude/ImmovableBlock.java
+++ src/blockdude/ImmovableBlock.java
@@ -5,15 +5,15 @@ package blockdude;
  * @author Henry Valentine Schreiber IV, Thomas Benjamin Clement, Jacob Thomas Dybas, Eric Spencer Lisle, Ethan Luke Harvey
  * This class extends Entity and is for blocks which cannot be moved
  */
-public class ImovableBlock extends Entity{
+public class ImmovableBlock extends Entity{
 	
 	/**
 	 * 
 	 * @param xPos the x coordinate of the top left corner
 	 * @param yPos the y coordinate of the top left corner
 	 */
-	public ImovableBlock(int xPos, int yPos) {
-		super(xPos, yPos, true, "ImoveableBlock.png");
+	public ImmovableBlock(int xPos, int yPos) {
+		super(xPos, yPos, true, "ImmoveableBlock.png");
 	}
 	
 }
